2487aedcc6a78124b205418987778b2a519e4102
Seohee  Im <seoheeim@vt.edu>
2020-07-22 12:33:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat(my_fav_things, all_my_favs)
 end 
 
 def using_insert(list_of_programming_languages, another_language)
-  puts list_of_programming_languages.insert(3, another_language)
+  list_of_programming_languages.insert(3, another_language)
 end
 
 def using_uniq(haircuts)
@@ -15,7 +15,7 @@ def using_flatten(instruments)
 end 
 
 def using_delete(instructors, no_offense_steven)
-  instructors.delet(no_offense_steven)
+  instructors.delete(no_offense_steven)
 end 
 
 def using_delete_at(famous_robots, number)
